34663576dd65ef20a84b8d14b17de0caff7e9e4e
shin-6-0 <syk6715@naver.com>
2023-09-08 11:25:02 +0900

    Action & ActionFactory #1

--- mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,54 +1,44 @@
 package com.poscodx.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.poscodx.mysite.dao.UserDao;
-import com.poscodx.mysite.vo.UserVo;
+import com.poscodx.mysite.web.mvc.user.JoinAction;
+import com.poscodx.mysite.web.mvc.user.JoinFormAction;
+import com.poscodx.mysite.web.mvc.user.JoinSuccessAction;
+import com.poscodx.web.mvc.Action;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		
-		String action = request.getParameter("a");
+		String actionName = request.getParameter("a");
+		Action action = null;
 		
-		if("joinform".equals(action)) {
-			request
-				.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
-				.forward(request, response);
-		}else if("join".equals(action)) {
-			String name=request.getParameter("name");
-			String email=request.getParameter("email");
-			String password=request.getParameter("password");
-			String gender=request.getParameter("gender");
-			
-			System.out.println(name+" : "+email+" : "+password+" : "+gender);
-			
-			UserVo userVo = new UserVo();
-			userVo.setName(name);
-			userVo.setEmail(email);
-			userVo.setGender(gender);
-			userVo.setPassword(password);
-			
-			new UserDao().insert(userVo);
-		}else {
-			
+		if("joinform".equals(actionName)) {
+			action = new JoinFormAction();
+		} else if("join".equals(actionName)) {
+			action = new JoinAction();
+		} else if("joinsuccess".equals(actionName)) {
+			action = new JoinSuccessAction();
 		}
+		
+		if(action == null) {
+			response.sendRedirect(request.getContextPath());
+			return;
+		}
+		
+		action.execute(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request,response);
+		doGet(request, response);
 	}
 
 }
---
+++ mysite02/src/main/java/com/poscodx/mysite/web/mvc/user/UserActionFactory.java
@@ -0,0 +1,7 @@
+package com.poscodx.mysite.web.mvc.user;
+
+import com.poscodx.web.mvc.ActionFactory;
+
+public class UserActionFactory implements ActionFactory {
+
+}
