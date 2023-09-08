635fc8ab728913c46f703fbb5bdf462ab60abf1e
shin-6-0 <syk6715@naver.com>
2023-09-08 09:58:50 +0900

    mysite02 - user.join#1

--- mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -6,6 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.poscodx.mysite.dao.UserDao;
+import com.poscodx.mysite.vo.UserVo;
+
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -18,10 +21,24 @@ public class UserController extends HttpServlet {
 		String action = request.getParameter("a");
 		
 		if("joinform".equals(action)) {
-			request.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
-					.forward(request, response);
-		}else if("joinfo".equals(action)) {
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/joinform.jsp")
+				.forward(request, response);
+		}else if("join".equals(action)) {
+			String name=request.getParameter("name");
+			String email=request.getParameter("email");
+			String password=request.getParameter("password");
+			String gender=request.getParameter("gender");
 			
+			System.out.println(name+" : "+email+" : "+password+" : "+gender);
+			
+			UserVo userVo = new UserVo();
+			userVo.setName(name);
+			userVo.setEmail(email);
+			userVo.setGender(gender);
+			userVo.setPassword(password);
+			
+			new UserDao().insert(userVo);
 		}else {
 			
 		}
@@ -31,7 +48,7 @@ public class UserController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		doGet(request,response);
 	}
 
 }
