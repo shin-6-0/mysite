b36ce41d036a9b9f131c9cd979b6f126a540dd92
shin-6-0 <syk6715@naver.com>
2023-09-18 11:01:40 +0900

    mysite03 - db ip 변경

--- mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -150,7 +150,7 @@ public class GuestbookRepository {
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 			
-			String url = "jdbc:mariadb://192.168.0.72:3307/webdb?charset=utf8";
+			String url = "jdbc:mariadb://192.168.0.92:3307/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
