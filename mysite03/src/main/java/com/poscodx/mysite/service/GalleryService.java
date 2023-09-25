f496eacd5f86065229e5a5e3d00cbf05b605061a
shin-6-0 <syk6715@naver.com>
2023-09-25 12:31:50 +0900

    mysite03 - Gallery MVC setting

---
+++ mysite03/src/main/java/com/poscodx/mysite/service/GalleryService.java
@@ -0,0 +1,22 @@
+package com.poscodx.mysite.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.poscodx.mysite.vo.GalleryVo;
+
+@Service
+public class GalleryService {
+	public List<GalleryVo> getImanges(){
+		return null;
+	}
+	
+	public void removeImange(Long no) {
+		
+	}
+	
+	public void addImange(GalleryVo vo) {
+		
+	}
+}
